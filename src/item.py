c675e996e33ace2e99961f301916f760d33848b0
afro_0 <kirillov2136@gmail.com>
2023-09-25 16:32:15 +0300

    hw2

--- src/item.py
+++ src/item.py
@@ -1,9 +1,12 @@
+import csv
+
+
 class Item:
     pay_rate = 1.0
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        self.name = name
+        self._name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
@@ -13,3 +16,28 @@ class Item:
 
     def apply_discount(self) -> None:
         self.price *= self.pay_rate
+
+    @property
+    def return_name(self):
+        return self._name
+
+    @return_name.setter
+    def return_name(self, new_name):
+        if isinstance(new_name, str):
+            self._name = new_name[0:11]
+
+    @classmethod
+    def instantiate_from_csv(cls, path):
+        with open(path, encoding="cp1251") as CSVfile:
+            read = csv.DictReader(CSVfile)
+            for row in read:
+                cls(row['name'], float(row['price']), int(row['quantity']))
+
+    @staticmethod
+    def string_to_number(string):
+        return int(string.split('.')[0])
+
+
+if __name__ == '__main__':
+    Item.instantiate_from_csv("items.csv")
+    print(Item.all[4].quantity)
--- tests/test_item.py
+++ tests/test_item.py
@@ -6,16 +6,36 @@ item1 = Item("Смартфон", 10000, 20)
 item2 = Item("Ноутбук", 20000, 5)
 
 
-def test_1_that_needs_resource():
+def test_1_hw1():
     assert item1.calculate_total_price() == 200000
     assert item2.calculate_total_price() == 100000
 
 
-def test_2_that_does_not():
+def test_2_hw1():
     Item.pay_rate = 0.8
     item1.apply_discount()
     item2.apply_discount()
 
     assert item1.price == 8000.0
     assert item2.price == 16000.0
+    assert item1.quantity == 20
+    assert item2.quantity == 5
 
+
+def test_3_hw2():
+    assert item1.return_name == "Смартфон"
+
+    item1.return_name = "Планшет"
+    assert item1.return_name == "Планшет"
+
+    Item.instantiate_from_csv('../src/items.csv')
+    assert len(Item.all) == 7
+    assert Item.all[5].price == 50.0
+    assert Item.all[6].quantity == 5
+
+
+def test_4_hw2():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+    assert Item.string_to_number('2340.567') == 2340
